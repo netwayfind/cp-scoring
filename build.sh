8dfcaf00dee2012ac35e3d879e478bd4dc688c3f
Mark <sumwonyuno@users.noreply.github.com>
2018-07-29 16:40:06 -1000

    update build script

--- build.sh
+++ build.sh
@@ -5,14 +5,16 @@ PKG_BASE="github.com/sumwonyuno/cp-scoring"
 echo "Fetching dependencies, this may take a while."
 go get github.com/mattn/go-sqlite3
 
-echo "Building linux agent"
-GOOS=linux GOARCH=amd64 go build -o cp-scoring-agent-linux $PKG_BASE/agent
 echo "Building linux server"
-GOOS=linux GOARCH=amd64 go build -o cp-scoring-server-linux $PKG_BASE/server
+mkdir -p cp-scoring-server
+GOOS=linux GOARCH=amd64 go build -o cp-scoring-server/cp-scoring-server-linux $PKG_BASE/server
 echo "Copying server UI files"
-rm -rf ./ui
-mkdir -p ui
-cp $(dirname $0)/server/ui/index.html ui
-cp $(dirname $0)/server/ui/index.js ui
+rm -rf cp-scoring-server/ui
+mkdir -p cp-scoring-server/ui
+cp $(dirname $0)/server/ui/index.html cp-scoring-server/ui
+cp $(dirname $0)/server/ui/index.js cp-scoring-server/ui
+echo "Building linux agent"
+mkdir -p cp-scoring-agent
+GOOS=linux GOARCH=amd64 go build -o cp-scoring-agent/cp-scoring-agent-linux $PKG_BASE/agent
 
 echo "Done"
