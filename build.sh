187d62b402814627a23665f00dcf49c49cbec3c8
Mark <sumwonyuno@users.noreply.github.com>
2018-08-02 20:33:29 -1000

    move ui to ui/admin

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ mkdir -p cp-scoring-server
 GOOS=linux GOARCH=amd64 go build -o cp-scoring-server/cp-scoring-server-linux $PKG_BASE/server
 echo "Copying server UI files"
 rm -rf cp-scoring-server/ui
-mkdir -p cp-scoring-server/ui
-cp $(dirname $0)/server/ui/index.html cp-scoring-server/ui
-cp $(dirname $0)/server/ui/index.js cp-scoring-server/ui
+mkdir -p cp-scoring-server/ui/admin
+cp $(dirname $0)/server/ui/admin/index.html cp-scoring-server/ui/admin
+cp $(dirname $0)/server/ui/admin/index.js cp-scoring-server/ui/admin
 echo "Building linux agent"
 mkdir -p cp-scoring-agent
 GOOS=linux GOARCH=amd64 go build -o cp-scoring-agent/cp-scoring-agent-linux $PKG_BASE/agent
