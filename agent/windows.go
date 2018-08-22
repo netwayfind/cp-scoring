ac3ba9c06ad90fea01e8d081cceda0c88487c4b4
Mark <sumwonyuno@users.noreply.github.com>
2018-08-21 20:49:47 -1000

    get software on windows

--- agent/windows.go
+++ agent/windows.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/sumwonyuno/cp-scoring/model"
+	"golang.org/x/sys/windows/registry"
 )
 
 func getState() model.State {
@@ -204,7 +205,84 @@ func getProcesses() []model.Process {
 }
 
 func getSoftware() []model.Software {
+	// based on add/remove programs
+	// first location
+	rights := uint32(registry.QUERY_VALUE | registry.ENUMERATE_SUB_KEYS | registry.QUERY_VALUE)
+	loc1Path := "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
+	loc1, err := registry.OpenKey(registry.LOCAL_MACHINE, loc1Path, rights)
+	if err != nil {
+		log.Fatal("ERROR: unable to get software;", err)
+	}
+	defer loc1.Close()
+	// second location
+	loc2Path := "SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
+	loc2, err := registry.OpenKey(registry.LOCAL_MACHINE, loc2Path, rights)
+	if err != nil {
+		log.Fatal("ERROR: unable to get software;", err)
+	}
+
 	software := make([]model.Software, 0)
+
+	// first location
+	subkeys, err := loc1.ReadSubKeyNames(-1)
+	if err != nil {
+		log.Fatal("ERROR: ", err)
+	}
+	for _, subkey := range subkeys {
+		key, err := registry.OpenKey(registry.LOCAL_MACHINE, loc1Path+"\\"+subkey, rights)
+		if err != nil {
+			log.Fatal("ERROR: cannot read key;", err)
+		}
+		vn, err := key.ReadValueNames(-1)
+		if err != nil {
+			log.Fatal("ERROR: cannot read key value names;", err)
+		}
+		if len(vn) == 0 {
+			continue
+		}
+		name, _, err := key.GetStringValue("DisplayName")
+		if err != nil {
+			continue
+		}
+		ver, _, err := key.GetStringValue("DisplayVersion")
+		if err != nil {
+			continue
+		}
+		var sw model.Software
+		sw.Name = name
+		sw.Version = ver
+		software = append(software, sw)
+	}
+	// second location
+	subkeys, err = loc2.ReadSubKeyNames(-1)
+	if err != nil {
+		log.Fatal("ERROR: ", err)
+	}
+	for _, subkey := range subkeys {
+		key, err := registry.OpenKey(registry.LOCAL_MACHINE, loc2Path+"\\"+subkey, rights)
+		if err != nil {
+			log.Fatal("ERROR: cannot read key;", err)
+		}
+		vn, _ := key.ReadValueNames(-1)
+		if err != nil {
+			log.Fatal("ERROR: cannot read key value names;", err)
+		}
+		if len(vn) == 0 {
+			continue
+		}
+		name, _, err := key.GetStringValue("DisplayName")
+		if err != nil {
+			continue
+		}
+		ver, _, err := key.GetStringValue("DisplayVersion")
+		if err != nil {
+			continue
+		}
+		var sw model.Software
+		sw.Name = name
+		sw.Version = ver
+		software = append(software, sw)
+	}
 
 	return software
 }
