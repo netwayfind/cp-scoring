ed16fb618e558f8e069d24b68631688b74bc7743
Mark <sumwonyuno@users.noreply.github.com>
2019-03-13 18:09:13 -1000

    [gh-16] add tests for state

--- server/db_test.go
+++ server/db_test.go
@@ -3,13 +3,18 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"strings"
 	"testing"
+
+	"github.com/sumwonyuno/cp-scoring/model"
 )
 
 var testDBObj backingStore
+var directDBConn *sql.DB
 
 func getTestBackingStore() (backingStore, error) {
 	if testDBObj == nil {
@@ -35,6 +40,12 @@ func getTestBackingStore() (backingStore, error) {
 			}
 		}
 
+		dbc, err := sql.Open("postgres", sqlURL)
+		if err != nil {
+			return testDBObj, err
+		}
+		directDBConn = dbc
+
 		tbs, err := getBackingStore("postgres", sqlURL)
 		if err != nil {
 			return testDBObj, err
@@ -42,6 +53,29 @@ func getTestBackingStore() (backingStore, error) {
 		testDBObj = tbs
 	}
 	return testDBObj, nil
+}
+
+func clearTables() error {
+	tx, err := directDBConn.Begin()
+	if err != nil {
+		return err
+	}
+	tx.Exec("TRUNCATE TABLE reports CASCADE")
+	tx.Exec("TRUNCATE TABLE scores CASCADE")
+	tx.Exec("TRUNCATE TABLE hosts_templates CASCADE")
+	tx.Exec("TRUNCATE TABLE scenarios CASCADE")
+	tx.Exec("TRUNCATE TABLE states CASCADE")
+	tx.Exec("TRUNCATE TABLE team_host_tokens CASCADE")
+	tx.Exec("TRUNCATE TABLE host_tokens CASCADE")
+	tx.Exec("TRUNCATE TABLE hosts CASCADE")
+	tx.Exec("TRUNCATE TABLE templates CASCADE")
+	tx.Exec("TRUNCATE TABLE teams CASCADE")
+	tx.Exec("TRUNCATE TABLE admins CASCADE")
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func TestGetPostgresBackingStore(t *testing.T) {
@@ -52,5 +86,69 @@ func TestGetPostgresBackingStore(t *testing.T) {
 	}
 	if backingStore == nil {
 		t.Fatal("Expected postgres backing store to not be nil")
+	}
+}
+
+func TestInsertState(t *testing.T) {
+	backingStore, err := getTestBackingStore()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = clearTables()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	state := model.State{Hostname: "test"}
+	stateBytes, _ := json.Marshal(state)
+
+	// state, no existing host token
+	err = backingStore.InsertState(1000, "127.0.0.1", "host-token", stateBytes)
+	if err == nil {
+		t.Fatal("Expected error due to missing host token")
+	}
+
+	// state, existing host token
+	backingStore.InsertHostToken("host-token", 1000, "host", "127.0.0.1")
+	err = backingStore.InsertState(1000, "127.0.0.1", "host-token", stateBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rows, err := directDBConn.Query("SELECT * FROM states")
+	if err != nil {
+		t.Fatal(err)
+	}
+	counter := 0
+	var timestamp int64
+	var source string
+	var hostToken string
+	var readStateBytes []byte
+	var readState model.State
+	for rows.Next() {
+		err = rows.Scan(&timestamp, &source, &hostToken, &readStateBytes)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if timestamp != 1000 {
+			t.Fatal("Unexpected timestamp value")
+		}
+		if source != "127.0.0.1" {
+			t.Fatal("Unexpected source value")
+		}
+		if hostToken != "host-token" {
+			t.Fatal("Unexpected host token value")
+		}
+		// check bytes can be turned into State
+		err = json.Unmarshal(readStateBytes, &readState)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if readState.Hostname != "test" {
+			log.Fatal("Unexpected hostname from state")
+		}
+		counter++
+	}
+	if counter != 1 {
+		t.Fatal("Unexpected number of rows:", counter)
 	}
 }
