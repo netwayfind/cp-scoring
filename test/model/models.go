050f66812f18873cb3e8d85d5d8442154d073604
Mark <sumwonyuno@users.noreply.github.com>
2020-12-25 09:17:45 -1000

    move to constants.go

--- test/model/models.go
+++ test/model/models.go
@@ -1,32 +1,5 @@
 package model
 
-// ActionType asdf
-type ActionType string
-
-// asdf
-const (
-	ActionTypeExec      ActionType = "EXEC"
-	ActionTypeFileExist ActionType = "FILE_EXIST"
-	ActionTypeFileRegex ActionType = "FILE_REGEX"
-	ActionTypeFileValue ActionType = "FILE_VALUE"
-)
-
-// OperatorType asdf
-type OperatorType string
-
-// asdf
-const (
-	OperatorTypeEqual    OperatorType = "EQUAL"
-	OperatorTypeNotEqual OperatorType = "NOT_EQUAL"
-	OperatorTypeNil      OperatorType = "NIL"
-	OperatorTypeNotNil   OperatorType = "NOT_NIL"
-)
-
-// asdf
-const (
-	KeyCharset string = "0123456789ABCDEF"
-)
-
 // Action asdf
 type Action struct {
 	Type    ActionType
